dbc1459cf84dda940ee7b1bb7e8f26f99dc06a50
herrfischerhamburg <46085157+herrfischerhamburg@users.noreply.github.com>
2020-01-20 23:17:07 +0100

    some

--- template_parts/modules_wrapper_container.php
+++ template_parts/modules_wrapper_container.php
@@ -8,7 +8,7 @@
             if ( get_sub_field('w_bgcolor') ) : ?>
             <?php $w_bgcolor = get_sub_field('w_bgcolor'); ?>
             <?php else : ?>
-            <?php $w_bgcolor = 0; ?>
+            <?php $w_bgcolor = "wrapper_nobgcolor"; ?>
         <?php endif; ?>
 
         <?php
@@ -26,11 +26,11 @@
             <?php /* Wrapper */ ?>
             class="
             wrapper
-            <?php if ($w_bgcolor !== 0) { ?>
-            wrapper_vspace_big
+            <?php if ($w_bgcolor == 'wrapper_nobgcolor') { ?>
+                wrapper_nobgcolor
             <?php } ?>
             "
-            <?php if ($w_bgcolor !== 0) { ?>
+            <?php if ($w_bgcolor !== "wrapper_nobgcolor") { ?>
                 style="background-color: <?php echo $w_bgcolor; ?>";
             <?php } ?>
         >
@@ -58,55 +58,41 @@
                     <?php while ( have_rows( 'row_regular' ) ) : the_row(); ?>
                     <?php $gr_col = get_row_index(); ?>
 
-                        <?php $desktop = get_sub_field('desktop'); ?>
-                        <?php $laptop = get_sub_field('laptop'); ?>
-                        <?php $tablet = get_sub_field('tablet'); ?>
                         <?php $smartphones = get_sub_field('smartphones'); ?>
-                        <?php $offset_desktop = get_sub_field('offset_desktop'); ?>
-                        <?php $offset_laptop = get_sub_field('offset_laptop'); ?>
+                        <?php $tablet = get_sub_field('tablet'); ?>
+                        <?php $laptop = get_sub_field('laptop'); ?>
+                        <?php $desktop = get_sub_field('desktop'); ?>
+                        <?php $offset_smartphones = get_sub_field('offset_smartphones'); ?>
                         <?php $offset_tablet = get_sub_field('offset_tablet'); ?>
-                        <?php $offset_smartphones = get_sub_field('offset_smartphones'); ?>   
+                        <?php $offset_laptop = get_sub_field('offset_laptop'); ?>
+                        <?php $offset_desktop = get_sub_field('offset_desktop'); ?>
                         <?php $textalign = get_sub_field('col_text_align'); ?>
                         <?php $columnbgcolor = get_sub_field('col_bgcolor'); ?> 
                         <?php $columncolor = get_sub_field('col_color'); ?>
                         <?php $columnnopadding = get_sub_field('no_padding'); ?>
                         <?php $col_aos = get_sub_field('mcos_aos'); ?>
-                        <?php $paddingY_xs = get_sub_field('mdodrh_xs'); ?>
-                        <?php $paddingY_sm = get_sub_field('mdodrh_sm'); ?>
-                        <?php $paddingY_md = get_sub_field('mdodrh_md'); ?>
-                        <?php $paddingY_lg = get_sub_field('mdodrh_lg'); ?>
 
                         <?php if ( have_rows( 'column' ) ): ?>
 
-                            <div class="
-                                <?php echo $desktop; ?>
-                                <?php echo $offset_desktop; ?>
-                                <?php echo $laptop; ?>
-                                <?php echo $offset_laptop; ?>
+                            <div class="<?php echo $smartphones; ?>
+                                <?php if ( $offset_smartphones !== '0') { echo $offset_smartphones; } ?>
                                 <?php echo $tablet; ?>
-                                <?php echo $offset_tablet; ?>
-                                <?php echo $smartphones; ?>
-                                <?php echo $offset_smartphones; ?>
-                                <?php echo $textalign; ?>
-                                
-                                <?php echo 'gr_container'.$gr_container.'gr_row'.$gr_row.'gr_col'.$gr_col; ?>
-                            ">  
+                                <?php if ( $offset_tablet !== '0') { echo $offset_tablet; } ?>
+                                <?php echo $laptop; ?>
+                                <?php if ( $offset_laptop !== '0') { echo $offset_laptop; } ?>
+                                <?php echo $desktop; ?>
+                                <?php if ( $offset_desktop !== '0') { echo $offset_desktop; } ?>
+                                <?php echo $textalign; ?>                                
+                                <?php echo 'gr_container'.$gr_container.'gr_row'.$gr_row.'gr_col'.$gr_col; ?>">  
 
                             <div class="
                                 col_inner
-                                <?php if ( !empty($columnbgcolor)) { ?>col_columnbgcolor<?php } ?>
-                            ">
-
-                            <div class="
-                            col_inner_bgcolor
-                            "                             
-                            style="background-color: <?php echo $columnbgcolor; ?>">
+                                <?php if ( !empty($columnbgcolor)) { ?>col_inner_bgcolor<?php } ?>
+                                "
+                                <?php if ( !empty($columnbgcolor)) { ?>style="background-color: <?php echo $columnbgcolor; ?>"<?php } ?>
+                            >
                                 <div class="
-                                col_inner_bgcolor_content
-                                <?php echo 'paddingY_xs_'.$paddingY_xs; ?>
-                                <?php echo 'paddingY_sm_'.$paddingY_sm; ?>
-                                <?php echo 'paddingY_md_'.$paddingY_md; ?>
-                                <?php echo 'paddingY_lg_'.$paddingY_lg; ?>
+                                col_inner_content
                                 <?php if ($columnnopadding != '0') { echo "p-0"; } ?> 
                                 ">
                                     <div <?php if( $col_aos != '0') { echo 'data-aos="fade-up"'; }; ?>>
@@ -116,8 +102,6 @@
                                         <?php endwhile; ?>
                                     </div>
                                 </div>
-                            </div>
-
                             </div>
                             </div>  
 
@@ -164,14 +148,14 @@
                     <?php while ( have_rows( 'row' ) ) : the_row(); ?>
                     <?php $gr_col = get_row_index(); ?>
 
-                        <?php $desktop = get_sub_field('desktop'); ?>
-                        <?php $offset_desktop = get_sub_field('offset_desktop'); ?>
-                        <?php $laptop = get_sub_field('laptop'); ?>
-                        <?php $offset_laptop = get_sub_field('offset_laptop'); ?>
-                        <?php $tablet = get_sub_field('tablet'); ?>
-                        <?php $offset_tablet = get_sub_field('offset_tablet'); ?>
                         <?php $smartphones = get_sub_field('smartphones'); ?>
-                        <?php $offset_smartphones = get_sub_field('offset_smartphones'); ?>  
+                        <?php $tablet = get_sub_field('tablet'); ?>
+                        <?php $laptop = get_sub_field('laptop'); ?>
+                        <?php $desktop = get_sub_field('desktop'); ?>
+                        <?php $offset_smartphones = get_sub_field('offset_smartphones'); ?>
+                        <?php $offset_tablet = get_sub_field('offset_tablet'); ?>
+                        <?php $offset_laptop = get_sub_field('offset_laptop'); ?>
+                        <?php $offset_desktop = get_sub_field('offset_desktop'); ?>
                         <?php $textalign = get_sub_field('col_text_align'); ?>
                         <?php $columnbgcolor = get_sub_field('col_bgcolor'); ?> 
                         <?php $columncolor = get_sub_field('col_color'); ?>
@@ -196,19 +180,17 @@
                         
                         <?php if ( have_rows( 'column' ) ): ?>
 
-                            <div class="
-                                <?php echo $desktop; ?>
-                                <?php echo $offset_desktop; ?>
-                                <?php echo $laptop; ?>
-                                <?php echo $offset_laptop; ?>
+                            <div class="<?php echo $smartphones; ?>
+                                <?php if ( $offset_smartphones !== '0') { echo $offset_smartphones; } ?>
                                 <?php echo $tablet; ?>
-                                <?php echo $offset_tablet; ?>
-                                <?php echo $smartphones; ?>
-                                <?php echo $offset_smartphones; ?>
+                                <?php if ( $offset_tablet !== '0') { echo $offset_tablet; } ?>
+                                <?php echo $laptop; ?>
+                                <?php if ( $offset_laptop !== '0') { echo $offset_laptop; } ?>
+                                <?php echo $desktop; ?>
+                                <?php if ( $offset_desktop !== '0') { echo $offset_desktop; } ?>
                                 p-0
                                 <?php echo $textalign; ?>
-                                <?php echo 'gr_container'.$gr_container.'gr_row'.$gr_row.'gr_col'.$gr_col; ?>
-                            ">  
+                                <?php echo 'gr_container'.$gr_container.'gr_row'.$gr_row.'gr_col'.$gr_col; ?>">  
 
                             <div class="
                                 col_inner
